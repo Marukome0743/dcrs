72b5dd8b9a2a3b147c4846a68d29a691ffa410a1
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-12 14:59:20 +0900

    🎨style: update upload form and add footer

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="footer footer-center bg-base-300 p-4 text-base-content">
+      <aside>
+        <p>Copyright © 2024 - All right reserved by Open Up Group Inc.</p>
+      </aside>
+    </footer>
+  )
+}
