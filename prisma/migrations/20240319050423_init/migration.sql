c431f37e389b24d69c11b87c293ee451c3fd0ac8
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-19 14:20:36 +0900

    ✨feat: add prisma

---
+++ prisma/migrations/20240319050423_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
+    "employeeId" INTEGER NOT NULL,
+    "phoneNumber" INTEGER NOT NULL,
+    "email" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
