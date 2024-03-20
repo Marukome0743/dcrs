a1617ee6dbe53e5289cc722533f6890347d5ce60
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-20 17:39:10 +0900

    ✨feat: add scroll to top button

---
+++ app/components/scrollToTop.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { ChevronDoubleUpIcon } from '@heroicons/react/24/solid'
+import { useState } from 'react'
+
+export function ScrollToTop() {
+  const [isScrollToTop, setIsScrollToTop] = useState(false)
+  const SCROLL_POINT = 200
+
+  window.onscroll = () => setIsScrollToTop(SCROLL_POINT < window.scrollY)
+
+  return (
+    <>
+      {isScrollToTop && (
+        <button
+          type="button"
+          className="btn btn-square btn-primary fixed right-10 bottom-10"
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          <ChevronDoubleUpIcon className="h-8 w-8" />
+        </button>
+      )}
+    </>
+  )
+}
