6853b74c8f7b7812b2950770ca13b3b4ea80d573
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-03-26 14:11:56 +0900

    🚑fix: fix smartphone design and miscellaneous things

--- app/components/homeBtn.tsx
+++ app/components/homeBtn.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export function HomeBtn() {
   return (
-    <Link href="/" className="btn btn-primary my-4">
+    <Link href="/" className="btn btn-primary">
       <HomeIcon className="h-6 w-6" />
       ホームに戻る
     </Link>
