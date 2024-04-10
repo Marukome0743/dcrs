9396fae431d648224c00960e1230c8dde809cc87
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-04-10 09:08:27 +0900

    ♻️refactor: amazon s3 upload functions

---
+++ app/api/image/[key]/route.ts
@@ -0,0 +1,11 @@
+import { client } from '@/app/lib/s3client'
+import { GetObjectCommand } from '@aws-sdk/client-s3'
+
+export async function GET({ params }: { params: { key: string } }) {
+  const command = new GetObjectCommand({
+    Bucket: process.env.S3_BUCKET,
+    Key: params.key,
+  })
+  const response = await client.send(command)
+  return Response.json({ response })
+}
