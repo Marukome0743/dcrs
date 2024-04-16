e9b63e65429c78a0b63686c1826efd6a5cb177ea
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-04-16 18:21:53 +0900

    ✨feat: add getBaseUrl library

---
+++ app/lib/getBaseUrl.ts
@@ -0,0 +1,12 @@
+export function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    return ''
+  }
+  if (process.env.NEXT_PUBLIC_VERCEL_URL) {
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  }
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`
+  }
+  return process.env.NEXT_PUBLIC_API_URL
+}
