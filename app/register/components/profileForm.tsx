e3123f300d36915e53e3e30ccd79a088b4d1deb0
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2024-06-13 14:16:02 +0900

    ♻️refactor: clean up constant values

--- app/register/components/profileForm.tsx
+++ app/register/components/profileForm.tsx
@@ -2,64 +2,13 @@
 
 import type { FormItem } from "@/app/interfaces/formItem"
 import type { Profile } from "@/app/interfaces/profile"
-import {
-  CheckIcon,
-  EnvelopeIcon,
-  IdentificationIcon,
-  PhoneIcon,
-  UserIcon,
-} from "@heroicons/react/24/solid"
+import { checklist } from "@/app/lib/constant"
+import { CheckIcon } from "@heroicons/react/24/solid"
 import type React from "react"
 import { useRef } from "react"
 import { type Path, type UseFormRegister, useForm } from "react-hook-form"
 import { ConfirmDialog } from "./confirmDialog"
 import { ImageUploader } from "./imageUploader"
-
-const checklist: FormItem[] = [
-  {
-    name: "name",
-    value: "氏名",
-    type: "text",
-    icon: UserIcon,
-    placeholder: "オープン太郎",
-  },
-  {
-    name: "company",
-    value: "所属会社",
-    type: "select",
-  },
-  {
-    name: "employeeId",
-    value: "社員番号",
-    type: "number",
-    icon: IdentificationIcon,
-    placeholder: "123456",
-  },
-  {
-    name: "telephone",
-    value: "電話番号",
-    type: "tel",
-    icon: PhoneIcon,
-    placeholder: "09012345678",
-  },
-  {
-    name: "email",
-    value: "Eメール",
-    type: "email",
-    icon: EnvelopeIcon,
-    placeholder: "example@mail.com",
-  },
-  {
-    name: "agreement",
-    value: "個人情報提供への同意",
-    type: "checkbox",
-  },
-  {
-    name: "image",
-    value: "障がい者手帳の画像・写真",
-    type: "file",
-  },
-] as const
 
 const COMPANIES: string[] = [
   "オープンアップグループ",
@@ -121,19 +70,10 @@ export function ProfileForm(): React.JSX.Element {
           <p className="mb-2 after:ml-0.5 after:text-red-500 after:content-['*']">
             {checklist.find((item) => item.name === "agreement")?.value}
           </p>
-          <p className="text-sm">
-            お預かりした個人情報は、株式会社オープンアップグループ（以下「当社」）が業務に利用するほか、
-            当社のグループ企業において、以下の利用目的で共同利用します。
-            <br />
-            共同利用についての公表事項及び共同利用者の範囲に含まれるグループ企業の一覧は、
-            以下の個人情報保護方針の記載をご覧下さい。
-            <br />
-            株式会社オープンアップグループは、提供頂いた個人情報を法令及び個人情報保護方針に従って
-            安全且つ適切に取り扱います。
-            <br />
-            個人情報に関する問い合わせ・請求方法等につきましては、「個人情報について」をご覧下さい。
-            <br />
-            上記の内容について同意頂ける方は、以下の「同意する」にチェックをお願い致します。
+          <p className="text-sm whitespace-pre">
+            {
+              "お預かりした個人情報は、\n株式会社オープンアップグループ\n（以下「当社」）が業務に利用するほか、\n当社のグループ企業において、\n以下の利用目的で共同利用します。\n\n共同利用についての公表事項及び\n共同利用者の範囲に含まれる\nグループ企業の一覧は、\n以下の個人情報保護方針の\n記載をご覧下さい。\n\n株式会社オープンアップグループは、\n提供頂いた個人情報を法令及び\n個人情報保護方針に従って\n安全且つ適切に取り扱います。\n\n個人情報に関する問い合わせ・\n請求方法等につきましては、\n「個人情報について」をご覧下さい。\n\n上記の内容について同意頂ける方は、\n以下の「同意する」にチェックを\nお願い致します。\n"
+            }
           </p>
           <label className="label cursor-pointer self-center">
             <span className="label-text mr-2">同意する</span>
@@ -152,7 +92,7 @@ export function ProfileForm(): React.JSX.Element {
         <CheckIcon className="size-6" />
         確認画面へ
       </button>
-      <ConfirmDialog dialog={dialog} checkList={[...checklist]} watch={watch} />
+      <ConfirmDialog dialog={dialog} watch={watch} />
     </form>
   )
 }
