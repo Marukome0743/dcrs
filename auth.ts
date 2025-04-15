95f151f20aba1f21a90e443af708dcaad79e0ea0
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-04-15 11:18:51 +0900

    🚑fix: login function

--- auth.ts
+++ auth.ts
@@ -1,17 +1,21 @@
 import { db } from "@/app/lib/schema"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import NextAuth from "next-auth"
-import ForwardEmail from "next-auth/providers/forwardemail"
+import Resend from "next-auth/providers/resend"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  callbacks: {
-    signIn({ profile }) {
-      if (!profile?.email) {
-        return false
-      }
-      return profile.email.endsWith("@bnt.benextgroup.jp")
-    },
-  },
-  providers: [ForwardEmail],
+  // callbacks: {
+  //   signIn({ profile }) {
+  //     if (!profile?.email) {
+  //       return false
+  //     }
+  //     return profile.email.endsWith("@bnt.benextgroup.jp")
+  //   },
+  // },
+  providers: [
+    Resend({
+      from: "lab.takizawa@openuplab-takizawa.com",
+    }),
+  ],
 })
