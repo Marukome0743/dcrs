b6d46060ea378ab62c7b1af56c69c13802d57831
Marukome0743 <akio.muto@bnt.benextgroup.jp>
2025-04-22 17:30:34 +0900

    🚀feat: use neon database for edge runtime

--- auth.ts
+++ auth.ts
@@ -2,14 +2,35 @@ import { db } from "@/app/lib/schema"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import NextAuth from "next-auth"
 import Resend from "next-auth/providers/resend"
-import authConfig from "./auth.config"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  ...authConfig,
   adapter: DrizzleAdapter(db),
   providers: [
     Resend({
       from: "lab.takizawa@openuplab-takizawa.com",
     }),
   ],
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isOnUserTable = nextUrl.pathname.startsWith("/users")
+      if (isOnUserTable) {
+        if (isLoggedIn) {
+          return true
+        }
+        return false
+      }
+      if (isLoggedIn) {
+        return Response.redirect(new URL("/users", nextUrl))
+      }
+      return true
+    },
+    signIn(profile) {
+      if (!profile?.user?.email) {
+        return false
+      }
+      return profile.user.email.endsWith("@bnt.benextgroup.jp")
+    },
+  },
+  trustHost: true,
 })
